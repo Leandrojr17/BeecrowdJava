a6c10afe948eb76d0785e71f8a51fd9371ef0571
Leandrojr17 <145725147+Leandrojr17@users.noreply.github.com>
2024-03-14 15:24:51 -0300

    Salário com Bônus

--- B1009.java
+++ B1009.java
@@ -4,13 +4,13 @@ public class B1009 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Qual o nome do funcionário?");
+        //System.out.println("Qual o nome do funcionário?");
         String nome = sc.next();
 
-        System.out.println("Qual o seu salário?");
+        //System.out.println("Qual o seu salário?");
         double salario = sc.nextDouble();
 
-        System.out.println("Qual o seu total de vendar efetuadas no mês?");
+        //System.out.println("Qual o seu total de vendar efetuadas no mês?");
         double vendas = sc.nextDouble();
         double comissao = vendas*0.15;
         double salarioFinal = comissao+salario;
