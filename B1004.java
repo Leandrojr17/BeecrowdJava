cbb93e1cc5fa9486eb9b8be96f54899b0b572e89
Leandrojr17 <145725147+Leandrojr17@users.noreply.github.com>
2024-03-14 15:18:55 -0300

    Produto Simples

--- B1004.java
+++ B1004.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teste = new Scanner(System.in);
-        System.out.println("Informe o valor de A");
+        //System.out.println("Informe o valor de A");
         int valorDeA = teste.nextInt();
-        System.out.println("Informe o valor de B");
+        //System.out.println("Informe o valor de B");
         int valorDeB = teste.nextInt();
         int x = valorDeA * valorDeB;
         System.out.println("PROD = "+ x);
